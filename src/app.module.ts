be1a8cc90a3fd12ea6a603a5d2115ff366d5a843
Oussama <oussama@notifyai.io>
2024-06-29 18:03:09 +0400

    remove unsued files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
@@ -24,7 +22,5 @@ import { AuthModule } from './auth/auth.module';
     ClientsModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
