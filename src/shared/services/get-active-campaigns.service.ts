d0192fcf3d2e3dcb85c1d22608c2f90f777e2e68
Oussama <oussama@notifyai.io>
2024-07-31 00:19:23 +0400

    build cache on call atm

--- src/shared/services/get-active-campaigns.service.ts
+++ src/shared/services/get-active-campaigns.service.ts
@@ -1,20 +1,39 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { RedisClientType } from 'redis';
+import { CampaignsService } from 'src/campaigns/campaigns.service';
 import { Campaign } from 'src/campaigns/entities/campaign.entity';
 
 @Injectable()
 export class GetActiveCampaignsService {
   constructor(
     @Inject('REDIS_CLIENT') private readonly redisClient: RedisClientType,
+    private campService: CampaignsService,
   ) {}
 
   async getActiveCampaigns(): Promise<Campaign> {
     const cachedValue = await this.redisClient.get('active-campaigns');
-    if (!cachedValue) {
-      return null;
+    if (cachedValue) {
+      const activeCampaigns = JSON.parse(cachedValue);
+      return activeCampaigns[
+        Math.floor(Math.random() * activeCampaigns.length)
+      ];
     }
-    const activeCampaigns: Campaign[] = JSON.parse(cachedValue);
+    // TODO: call the build campaign cache dont do it here
+    const activeCampaigns = await this.campService.findAll([
+      'lander',
+      'advertiser',
+    ]);
 
-    return activeCampaigns[Math.floor(Math.random() * activeCampaigns.length)];
+    if (activeCampaigns.length > 0) {
+      await this.redisClient.set(
+        'active-campaigns',
+        JSON.stringify(activeCampaigns),
+      );
+      return activeCampaigns[
+        Math.floor(Math.random() * activeCampaigns.length)
+      ];
+    }
+
+    return null;
   }
 }
