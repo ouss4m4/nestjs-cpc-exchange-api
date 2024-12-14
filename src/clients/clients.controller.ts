70bb3f3c0fc000054de8a66b90cf22ad33e31037
Oussama <bz.oussama@gmail.com>
2024-12-14 13:18:33 +0400

    prep for docker setup

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -6,25 +6,26 @@ import {
   Patch,
   Param,
   Delete,
-  UseGuards,
+  Query,
 } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
-import { LocalAuthGuard } from 'src/auth/local-auth.guard';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('clients')
 export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
 
   @Post()
+  @ApiBody({ type: CreateClientDto })
   create(@Body() createClientDto: CreateClientDto) {
     return this.clientsService.create(createClientDto);
   }
-  @UseGuards(LocalAuthGuard)
+
   @Get()
-  findAll() {
-    return this.clientsService.findAll();
+  findAll(@Query() query: { type?: string; status?: string }) {
+    return this.clientsService.findAll(query);
   }
 
   @Get(':id')
