c9e964707ea7d4174229596c2bed1190abdb0ad6
Oussama <bz.oussama@gmail.com>
2024-12-18 21:56:53 +0400

    add /ping endpoint

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as os from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,12 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
   app.enableCors();
+  app.getHttpAdapter().get('/ping', (req, res) => {
+    res.status(200).send({
+      message: 'pong',
+      instance: os.hostname(),
+    });
+  });
   await app.listen(process.env.EXPRESS_PORT || 3000);
 }
 bootstrap();
