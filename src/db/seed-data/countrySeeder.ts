6b5ba1a5b177ae0beb8ad8a65a0f8a79410b3aa1
Oussama <bz.oussama@gmail.com>
2024-12-18 23:12:12 +0400

    add country seeder from csv

---
+++ src/db/seed-data/countrySeeder.ts
@@ -0,0 +1,34 @@
+import { DataSource } from 'typeorm';
+import { AppDataSource } from '../data-source';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { parse } from 'fast-csv';
+import { Country } from '../../countries/entities/country.entity';
+const seedCountries = async () => {
+  const dataSource: DataSource = await AppDataSource.initialize();
+  const countryRepo = dataSource.getRepository(Country);
+  const countryCsvStream = createReadStream(
+    join(__dirname, 'countries.csv'),
+  ).pipe(parse({ headers: true, ignoreEmpty: true }));
+
+  for await (const row of countryCsvStream) {
+    const country = countryRepo.create({
+      id: row.id,
+      iso: row.iso,
+      name: row.name,
+      niceName: row.nicename,
+    });
+    await countryRepo.save(country);
+  }
+  return 'Country seed success';
+};
+
+seedCountries()
+  .then((sf) => {
+    console.log(sf);
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(0);
+  });
